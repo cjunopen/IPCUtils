95a7e41eddad4aa3a07047c457a4119e0f5f8145
chenjun <853364235@qq.com>
2023-10-13 11:24:25 +0800

    添加 @RemoteInterface

--- ipc-library/src/main/java/com/github/cjunopen/ipc_library/interfaces/ILiYuHome.java
+++ ipc-library/src/main/java/com/github/cjunopen/ipc_library/interfaces/ILiYuHome.java
@@ -1,12 +1,14 @@
 package com.github.cjunopen.ipc_library.interfaces;
 
 import com.codezjx.andlinker.annotation.Callback;
+import com.codezjx.andlinker.annotation.RemoteInterface;
 
 /**
  * @Description:
  * @Author: CJ
  * @CreateDate: 2023/10/13 9:25
  */
+@RemoteInterface
 public interface ILiYuHome {
 
     String request(String req);
